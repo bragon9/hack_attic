b184cda1cd8a2c3e51ad1b623ce14e186cd2c202
Brandon Spagon <bspagon@iofficecorp.com>
2019-08-02 00:12:32 -0500

    Clean up answer and add comments.

--- helpmeunpack.py
+++ helpmeunpack.py
@@ -2,26 +2,19 @@ import requests
 from base64 import b64decode
 import struct
 
+# Get input data.
 response = requests.get('https://hackattic.com/challenges/help_me_unpack/problem?access_token=96d50621d8c2e08d')
-
 response_dict = response.json()
 
+# Get the base64 encoded string and decode.
 encoded_string = response_dict['bytes']
 decoded_string = b64decode(encoded_string)
 
-# int: the signed integer value
-# uint: the unsigned integer value
-# short: the decoded short value
-# float: surprisingly, the float value
-# double: the double value - shockingly
-# big_endian_double: you get the idea by now!
-
-# 'iIhfdd'
-
+# Unpack the data.
 unpacked_data = struct.unpack('iIhfdd', decoded_string)
+# print(unpacked_data)
 
-print(unpacked_data)
-
+# Send the answer.
 post_object = requests.post('https://hackattic.com/challenges/help_me_unpack/solve?access_token=96d50621d8c2e08d', 
     json = {
         'int':unpacked_data[0],
@@ -29,7 +22,8 @@ post_object = requests.post('https://hackattic.com/challenges/help_me_unpack/sol
         'short':unpacked_data[2],
         'float':unpacked_data[3],
         'double':unpacked_data[4],
-        'big_endian_double':unpacked_data[5]
+        'big_endian_double':unpacked_data[4]
         })
 
+# Check the answer.
 print(post_object.json())
